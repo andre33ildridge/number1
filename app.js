73b708a58413e0c38b91bb530b2ab050ab472886
and33ildridge <andy.wildridge.fr@gmail.com>
2015-01-14 13:44:00 +0000

    update

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+server.listen(8080);
